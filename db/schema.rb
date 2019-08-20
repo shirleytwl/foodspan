ec940367225138b861ce761eb8199807811af18e
FrederickKu <timothyfrederick.ku@gmail.com>
2019-08-20 11:57:10 +0800

    Created Tag Table and Ingredients_Tags Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_034810) do
+ActiveRecord::Schema.define(version: 2019_08_20_035555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,21 @@ ActiveRecord::Schema.define(version: 2019_08_20_034810) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "purchase_date"
+  end
+
+  create_table "ingredients_tags", force: :cascade do |t|
+    t.bigint "ingredient_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_ingredients_tags_on_ingredient_id"
+    t.index ["tag_id"], name: "index_ingredients_tags_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

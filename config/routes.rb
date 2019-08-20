d21a91ac9847f6676e80704e912818d92d571aeb
shirleytwl <shirtanwl@gmail.com>
2019-08-20 17:01:46 +0800

    initialise route, controller and view for storage

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/storage' => 'storages#index', as: 'storages'
+
   get '/suggestion' => 'suggestions#index', as: 'suggestions'
 end

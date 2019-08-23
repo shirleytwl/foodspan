8150371a7e6479b8f0467b13861c191c3c8aede1
FrederickKu <timothyfrederick.ku@gmail.com>
2019-08-23 12:20:38 +0800

    Working graph and tooltip

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,6 @@
 class ReportsController < ApplicationController
   def index
     tags = Tag.all
-
     @details = []
 
     tags.each do |tag|
@@ -15,6 +14,7 @@ class ReportsController < ApplicationController
         tag.ingredients.each do |ingredient|
           if ingredient.user == current_user && ingredient.removed == true && ingredient.updated_at >= Date.today - 1.year && ingredient.updated_at < Date.today
             prep.push(ingredient.quantity_left.to_f/ingredient.quantity.to_f)
+            data[:tag] = tag
           end
         end
       when 'quarter'
@@ -22,6 +22,7 @@ class ReportsController < ApplicationController
         tag.ingredients.each do |ingredient|
           if ingredient.user == current_user && ingredient.removed == true && ingredient.updated_at >= Date.today - 3.month && ingredient.updated_at < Date.today
             prep.push(ingredient.quantity_left.to_f/ingredient.quantity.to_f)
+            data[:tag] = tag
           end
         end
       when 'month'
@@ -29,6 +30,7 @@ class ReportsController < ApplicationController
         tag.ingredients.each do |ingredient|
           if ingredient.user == current_user && ingredient.removed == true && ingredient.updated_at >= Date.today - 1.month && ingredient.updated_at < Date.today
             prep.push(ingredient.quantity_left.to_f/ingredient.quantity.to_f)
+            data[:tag] = tag
           end
         end
       when 'week'
@@ -36,6 +38,7 @@ class ReportsController < ApplicationController
         tag.ingredients.each do |ingredient|
           if ingredient.user == current_user && ingredient.removed == true && ingredient.updated_at >= Date.today - 1.week && ingredient.updated_at < Date.today
             prep.push(ingredient.quantity_left.to_f/ingredient.quantity.to_f)
+            data[:tag] = tag
           end
         end
       when 'day'
@@ -43,6 +46,7 @@ class ReportsController < ApplicationController
         tag.ingredients.each do |ingredient|
           if ingredient.user == current_user && ingredient.removed == true && ingredient.updated_at >= Date.today - 1.day && ingredient.updated_at < Date.today
             prep.push(ingredient.quantity_left.to_f/ingredient.quantity.to_f)
+            data[:tag] = tag
           end
         end
       else
@@ -50,6 +54,7 @@ class ReportsController < ApplicationController
         tag.ingredients.each do |ingredient|
           if ingredient.user == current_user && ingredient.removed == true
             prep.push(ingredient.quantity_left.to_f/ingredient.quantity.to_f)
+            data[:tag] = tag
           end
         end
       end
@@ -68,5 +73,71 @@ class ReportsController < ApplicationController
       end
 
     end
+  end
+
+  def show
+    tag=Tag.find(params[:id])
+
+    @data=scatterChart(tag)
+    @options = scatterOptions
+
+  end
+
+  private def scatterChart (tag)
+    details={:datasets => []}
+
+    tag.ingredients.each do |ingredient|
+      data = {:label => ingredient.name, :borderColor=> '', :backgroundColor => '', :data => [{:x => ((ingredient.quantity_left.to_f/ingredient.quantity.to_f)*100).round(2)}]}
+      data[:data].first[:y] = 100.0 - data[:data].first[:x]
+
+      if data[:data].first[:x] > 20
+        data[:borderColor] = '#346102'
+        data[:backgroundColor] = 'rgba(111, 150, 55, 1)'
+      else
+        data[:borderColor] = '#802E00'
+        data[:backgroundColor] = 'rgba(190, 94, 40, 1)'
+      end
+
+      details[:datasets].push(data)
+    end
+    return details
+  end
+
+  private def scatterOptions
+    options = {
+      :responsive => true,
+      :maintainAspectRatio => false,
+      :width => 300,
+      :height => 300,
+      :legend => {
+            :display => false
+      },
+      :scales => {
+        :yAxes => [{
+          :scaleLabel => {
+            :display => true,
+            :labelString => "Food Waste(%)"
+          }
+        }],
+        :xAxes => [{
+          :scaleLabel => {
+            :display => true,
+            :labelString => "Food Consumed(%)"
+          }
+        }]
+      },
+      :tooltips => {
+        :callbacks => {
+          :label => "function(tooltipItem, data) {
+            var label = data.datasets[tooltipItem.datasetIndex].label;
+            return label;
+          }",
+          :afterLabel => "function(tooltipItem, data) {
+            var label = ['Wasted(%): ' + tooltipItem.yLabel  + '%', 'Consumed(%): ' + tooltipItem.xLabel + '%'];
+            return label;
+          }"
+        }
+      }
+    }
   end
 end
